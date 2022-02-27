bfada36a78ef9b144f14db3b831fa3fa82a8c8e6
Abraham Ramirez <abrahammoreno044@gmail.com>
2022-02-26 22:11:41 -0600

    Cards con nombres de estudiantes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,61 @@ export class Tab1Page {
 
   constructor() {}
 
+  alumnos = [
+    {
+      "nombre": "Abraham",
+      "Apellido": "Ramírez",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Aylin",
+      "Apellido": "Demecti",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Brian",
+      "Apellido": "Esquivel",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Diego",
+      "Apellido": "Dávila",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Diego",
+      "Apellido": "Jasso",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Julio",
+      "Apellido": "Manuel",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Luis",
+      "Apellido": "Armando",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Luis",
+      "Apellido": "Otoniel",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Manuel",
+      "Apellido": "Antonio",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Rogel",
+      "Apellido": "Axel",
+      "matricula": "ABC123",
+    },
+    {
+      "nombre": "Sergio",
+      "Apellido": "Gutierrez",
+      "matricula": "ABC123",
+    }
+  ]
 }
